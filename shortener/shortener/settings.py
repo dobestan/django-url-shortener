8ae0a544179caea239caa775b41fe3f06d28bfc4
dobestan <dobestan@gmail.com>
2014-08-16 11:47:59 +0900

    update settings.py : update INSTALLED_APPS

--- shortener/shortener/settings.py
+++ shortener/shortener/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom Generated Apps
+    'links',
 )
 
 MIDDLEWARE_CLASSES = (
