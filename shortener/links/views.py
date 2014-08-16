4d728d9e6628c4dfa4f80a456485f709f95903ac
dobestan <dobestan@gmail.com>
2014-08-16 13:34:41 +0900

    add feature redirect : using RedirectView

--- shortener/links/views.py
+++ shortener/links/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from django.views.generic.base import RedirectView
+from links.models import Link
+
+class LinkRedirect(RedirectView):
+    def get_redirect_url(self, shorten):
+        link = get_object_or_404(Link, shorten = shorten)
+        return link.original
--- shortener/shortener/urls.py
+++ shortener/shortener/urls.py
@@ -3,6 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+# Custom View
+from links.views import LinkRedirect
+
 urlpatterns = patterns('',
     url(r'^api/', include('API.urls', namespace="API")),
+    url(r'^(?P<shorten>[0-9a-zA-Z]+)/$', LinkRedirect.as_view(), name="link-redirect"),
 )
